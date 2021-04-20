411040da8c9786a5cfebe34a52410f1ba8814812
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-04-20 11:54:57 +0300

    update

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
